099b3e14d6c702af13dae5b091aab81c0d980c99
Anna Korzh <anna.korzh@valtech.com>
2022-06-23 09:18:58 +0300

    assignment-3

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,18 +7,29 @@ import Card from '../UI/Card';
 import './Expenses.css';
 
 const selectYear = (setYear, value) => {
-    setYear(value);
-}
+  setYear(value);
+};
 
 export const Expenses = ({ expenses }) => {
-    const [yearToFilter, setYearToFilter] = useState('2022');
+  const [yearToFilter, setYearToFilter] = useState('2022');
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.getFullYear() === parseInt(yearToFilter)
+  );
 
-    return (
-        <Card className='expenses'>
-            <ExpensesFilter selected={yearToFilter} onSelectYear={selectYear.bind(this, setYearToFilter)}/>
-            {expenses.map((expense) =>
-                <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-            )}
-        </Card>
-    );
-}
+  return (
+    <Card className='expenses'>
+      <ExpensesFilter
+        selected={yearToFilter}
+        onSelectYear={selectYear.bind(this, setYearToFilter)}
+      />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+};
