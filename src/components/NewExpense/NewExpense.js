b833378ab3d290dce8506e675593eb5843d4586f
Anna Korzh <anna.korzh@valtech.com>
2022-06-23 11:20:48 +0300

    improvements to assignment 4

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,4 +1,8 @@
-import NewExpenseContent from './NewExpenseContent';
+import { useState } from 'react';
+
+import ExpenseForm from './ExpenseForm';
+
+import { handleEvent } from '../../utils';
 import './NewExpense.css';
 
 const saveExpenseDataHandler = (onAddExpense, enteredExpenseData) => {
@@ -11,12 +15,28 @@ const saveExpenseDataHandler = (onAddExpense, enteredExpenseData) => {
   onAddExpense(expenseData);
 };
 
+const toggleExpenseFormHandler = (e, setVisible) =>
+  setVisible((prevState) => !prevState);
+
 export const NewExpense = ({ onAddExpense }) => {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const toggleExpenseForm = handleEvent(
+    toggleExpenseFormHandler,
+    setIsFormVisible
+  );
+
   return (
     <div className='new-expense'>
-      <NewExpenseContent
-        onSaveExpenseData={saveExpenseDataHandler.bind(this, onAddExpense)}
-      />
+      {isFormVisible ? (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler.bind(this, onAddExpense)}
+          toggleExpenseFormHandler={toggleExpenseForm}
+        />
+      ) : (
+        <button type='button' onClick={toggleExpenseForm}>
+          Add New Expense
+        </button>
+      )}
     </div>
   );
 };
