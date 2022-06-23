262ffaa076dee74c622d5bf0e56ab347bf1b96c5
Anna Korzh <anna.korzh@valtech.com>
2022-06-23 09:06:00 +0300

    improvements in between 2 and 3 assignments

--- src/App.js
+++ src/App.js
@@ -4,43 +4,38 @@ import { Expenses } from './components/Expenses/Expenses';
 import { NewExpense } from './components/NewExpense/NewExpense';
 
 const addExpenseHandler = (setExpenses, expense) => {
-    setExpenses((prevState) => {
-        return [
-            ...prevState,
-            expense
-        ]
-    });
-}
+  setExpenses((prevState) => [expense, ...prevState]);
+};
 
 const App = () => {
-    const [expenses, setExpenses] = useState([
-        {
-            id: 'e1',
-            title: 'Toilet Paper',
-            amount: 4.12,
-            date: new Date(2020, 7, 14),
-        },
-        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ]);
+  const [expenses, setExpenses] = useState([
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 4.12,
+      date: new Date(2020, 7, 14)
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12)
+    }
+  ]);
 
-    return (
-        <div>
-            <NewExpense onAddExpense={addExpenseHandler.bind(this, setExpenses)} />
-            <Expenses expenses={expenses} />
-        </div>
-    );
-}
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler.bind(this, setExpenses)} />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -5,12 +5,12 @@ const selectYearHandler = (e, setYear) => {
     setYear(e.target.value);
 }
 
-export const ExpensesFilter = ({ onSelectYear }) => {
+export const ExpensesFilter = ({ selected, onSelectYear }) => {
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={handleEvent(selectYearHandler, onSelectYear)}>
+        <select value={selected} onChange={handleEvent(selectYearHandler, onSelectYear)}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
